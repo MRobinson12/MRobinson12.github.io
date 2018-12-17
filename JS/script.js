542fdbca0d019a7d5269cef7999334a157b24ebd
Morgan <43744780+MRobinson12@users.noreply.github.com>
2018-12-17 15:51:27 +0000

    JS on CD Creation beginning

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,12 @@
-$(document).ready(function(){
-    console.log("Ready")});
+window.onload=function(){console.log("Ready")};
+
+var slotselection = null;
+
+//Selects the song slot to change
+$('.songbutton').click(function()
+    {
+        $('.songbutton').removeClass('selected');
+        $(this).addClass('selected');
+        console.log($(this).attr('id') +" has been selected");
+        slotselection =  $(this).attr('id');
+    });
