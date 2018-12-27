ad3f6d4357bd2cdf585f52b52834f38b8a67b860
Morgan <43744780+MRobinson12@users.noreply.github.com>
2018-12-27 14:14:49 +0000

    CSS changes and java additions

--- JS/script.js
+++ JS/script.js
@@ -4,9 +4,21 @@ var slotselection = null;
 
 //Selects the song slot to change
 $('.songbutton').click(function()
+{
+    $('.songbutton').removeClass('selected');
+    $(this).addClass('selected');
+    console.log($(this).attr('id') +" has been selected");
+    slotselection =  $(this).attr('id');
+});
+
+$('.albumcover').mouseover(function()
+{
+    $('.albumcover').removeClass('highlighted');
+    $(this).addClass('highlighted');
+    console.log($(this).attr('id') +" has been highlighted");
+    slotselection =  $(this).attr('id');
+});
+$('.albumcover').mouseout(function()
     {
-        $('.songbutton').removeClass('selected');
-        $(this).addClass('selected');
-        console.log($(this).attr('id') +" has been selected");
-        slotselection =  $(this).attr('id');
+    $('.albumcover').removeClass('highlighted');    
     });
