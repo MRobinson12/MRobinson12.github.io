37bdac45149e9f11872e4f6714a8ee2e0a694e46
Morgan <43744780+MRobinson12@users.noreply.github.com>
2018-12-30 15:04:22 +0000

    Start of Mob CSS and CD cover changer

--- JS/script.js
+++ JS/script.js
@@ -38,3 +38,37 @@ $('.navbutton').mouseout(function()
     {
     $('.navbutton').removeClass('selected');    
     });
+
+//CD creator variables
+var albumselector = 0;
+var albumcover = ["Assets/album1.jpg", "Assets/album2.jpg", "Assets/album3.jpg", "Assets/album4.jpg", "Assets/album5.jpg", "Assets/album6.jpg", "Assets/album7.jpg", "Assets/album8.jpg"]
+
+//Changes the CD cover image
+$('#nextcover').click(function()
+    {
+    if (albumselector == 7)
+    {
+        albumselector = 0;
+        $('#CustomAlbumCover').attr('src',albumcover[albumselector]);
+    }
+    else
+    {
+        albumselector = albumselector + 1;
+        $('#CustomAlbumCover').attr('src',albumcover[albumselector]);
+    }
+});
+$('#prevcover').click(function()
+    {
+    if (albumselector == 0)
+    {
+        albumselector = 7;
+        $('#CustomAlbumCover').attr('src',albumcover[albumselector]);
+    }
+    else
+    {
+        albumselector = albumselector - 1;
+        $('#CustomAlbumCover').attr('src',albumcover[albumselector]);
+    }
+});
+
+
