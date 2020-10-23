6401f707da694e6e219c4707f44c72fcb64d3603
Morgan <morganrobinson1202@gmail.com>
2020-10-23 11:01:44 +0100

    newweeknewmeme

--- Ceph Test.sh
+++
@@ -1,8 +0,0 @@
-#1. Change the systemd unit file
-#    Opens Tickets and shuts down if a disk has restarted more than once
-#        Do nothing if restarts once
-#    Opens a Ticket if it goes down (stopped)
-#/etc/noquattor - Stops Aquilon changing settings
-#/usr/lib/systemd/system/
-
-cat /var/log/ceph/ceph-mon.$(hostname -s).log | grep -i "forcing failure" | awk '{ print $NF }' | sort | uniq -c
